715ba80786d0b79b1505d68dc2d72f57eb05efb2
= <=>
2020-08-11 11:17:10 +0000

    Third commit

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -6,6 +6,7 @@ import skimage.transform as sktransform
 import random
 import matplotlib.image as mpimg
 import shutil
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, MaxPool2D, Dropout, Flatten, Lambda
 from keras.optimizers import Adam
@@ -17,7 +18,7 @@ from datetime import datetime
 path = '/opt/carnd_p3/data'
 
 cameras = ['left', 'center', 'right']
-cameras_steering_correction = [-0.4, 0.0, 0.4]
+cameras_steering_correction = [0.25, 0.0, -0.25]
 
 def preprocess(image, top_offset=.375, bottom_offset=.125):
     """
@@ -35,19 +36,13 @@ def flip( x, y):
     y[flip_indices] = -y[flip_indices]
     return (x,y)
 
-def make_dataset( dataset):
+def make_dataset( dataset, train = True):
     n = int(len(dataset)/4)
     x = []
     y = []
     for i in range(len(dataset)):
         val = dataset.steering.values[i]
-
-        if val > 0.0:
-            j = 2
-        elif val==0.0:
-            j = 1
-        else:
-            j = 0
+        j = np.random.randint(len(cameras)) if train else 1
         #if i%n==0:
             #print(val)
             #print(cameras[j])
@@ -55,77 +50,86 @@ def make_dataset( dataset):
         y.append( val + cameras_steering_correction[j])
     x = np.array(x)#/127
     y = np.array(y)#/127
-    #return flip(x, y)
-    return (x, y)
+    return flip(x, y)
+    #return (x, y)
 
 if __name__ == '__main__':
-    
-    log = open("log.txt" , "a")
-    log.write(str(datetime.now().strftime('%c')))
-    
-    test_size = 0.3
-    
-    df = pd.read_csv(os.path.join(path, 'driving_log.csv'))
-    train, valid = model_selection.train_test_split(df, test_size=test_size)
-    train_x , train_y = make_dataset(train)
-    #print(len(train_y))
-    #print(sum(train_y > 0))
-    print("Training Data acquired!")
-    
-    valid_x , valid_y = make_dataset(valid)
-    print("Validation Data acquired!")
-    
-    print("Data acquired!")
 
-    # Model architecture from NVIDIA Research Paper
-    model = Sequential()
-    #model.add(Lambda(lambda x: x / 64 - 0.5, input_shape=(66, 200, 3)))
-    model.add(Conv2D(24, 5, strides = (2,2), input_shape=(66, 200, 3), activation='relu'))
-    model.add(Conv2D(36, 5, strides = (2,2), input_shape=(31, 98, 24), activation='relu'))
-    model.add(Conv2D(48, 5, strides = (2,2), input_shape=(14, 47, 36), activation='relu'))
-    model.add(Conv2D(64, 3, input_shape=(5, 22, 48), activation='relu'))
-    model.add(Conv2D(64, 3, input_shape=(3, 20, 64), activation='relu'))
-    model.add(Flatten())
-    model.add(Dense(1164, activation='relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation='relu'))
-    model.add(Dense(1))
-    model.build()
-    model.summary(print_fn = lambda x : log.write(x+'\n'))
-    model.compile(optimizer=Adam(lr=1e-04), loss= "mse")
-    
-    print("Model built!")
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=3, verbose=1, factor=0.2, min_lr=1e-6)
-    datagen = ImageDataGenerator(
-                rotation_range = 20,
-                width_shift_range = 0.2,\
-                height_shift_range = 0.2,\
-                brightness_range = (0.7,0.9)
-                )
-
-    datagen.fit(train_x)
-
-    history = model.fit_generator(
-        datagen.flow(train_x,train_y, batch_size = 128) ,
-        epochs=5,
-        validation_data= (valid_x, valid_y),
-        callbacks=[reduce_lr]
-    )
-    
     try:
+    
+        log = open("log.txt" , "a")
+        log.write(str(datetime.now().strftime('%c')))
+
+        test_size = 0.3
+
+        df = pd.read_csv(os.path.join(path, 'driving_log.csv'))
+        train, valid = model_selection.train_test_split(df, test_size=test_size)
+        train_x , train_y = make_dataset(train)
+        #print(len(train_y))
+        #print(sum(train_y > 0))
+        print("Training Data acquired!")
+
+        valid_x , valid_y = make_dataset(valid , False)
+        print("Validation Data acquired!")
+
+        print("Data acquired!")
+
+        # Model architecture from NVIDIA Research Paper
+        model = Sequential()
+        model.add(Lambda(lambda x: x / 64 - 0.5, input_shape=(66, 200, 3)))
+        model.add(Conv2D(24, 5, strides = (2,2), input_shape=(66, 200, 3), activation='relu'))
+        model.add(Conv2D(36, 5, strides = (2,2), input_shape=(31, 98, 24), activation='relu'))
+        model.add(Conv2D(48, 5, strides = (2,2), input_shape=(14, 47, 36), activation='relu'))
+        model.add(Conv2D(64, 3, input_shape=(5, 22, 48), activation='relu'))
+        model.add(Conv2D(64, 3, input_shape=(3, 20, 64), activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(1164, activation='relu'))
+        #model.add(Dropout(0.5))
+        model.add(Dense(100, activation='relu'))
+        model.add(Dense(50, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(10, activation='relu'))
+        model.add(Dense(1))
+        model.build()
+        model.summary(print_fn = lambda x : log.write(x+'\n'))
+        model.compile(optimizer=Adam(lr=1e-04), loss= "mse")
+
+        print("Model built!")
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=3, verbose=1, factor=0.2, min_lr=1e-6)
+        datagen = ImageDataGenerator(
+                    rotation_range = 20,
+                    width_shift_range = 0.2,\
+                    height_shift_range = 0.2,\
+                    brightness_range = (0.7,0.9)
+                    )
+
+        datagen.fit(train_x)
+
+        history = model.fit_generator(
+            datagen.flow(train_x,train_y, batch_size = 32) ,
+            epochs=12,
+            validation_data= (valid_x, valid_y),
+            callbacks=[reduce_lr]
+        )
+    
         log.write(str(history.history))
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['training loss', 'validation loss'], loc='upper left')
+        plt.savefig('model_loss.png')
+       
+        model.save("model.h5")
+
+        with open(os.path.join('./', 'model.json'), 'w') as file:
+            file.write(model.to_json())
+
+        log.write("\n=================================================================================\n\n")
+        log.close()
+
+        print("Finished!")
+    
     except Exception as e:
         log.write(e)
-    
-    model.save("model.h5")
-    
-    with open(os.path.join('./', 'model.json'), 'w') as file:
-        file.write(model.to_json())
-    
-    log.write("\n=================================================================================\n\n")
-    log.close()
-        
-    print("Finished!")
--- CarND-Behavioral-Cloning-P3/test.py
+++ CarND-Behavioral-Cloning-P3/test.py
@@ -1,4 +1,5 @@
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
@@ -12,7 +13,7 @@ path = '/opt/carnd_p3/data'
 
 if __name__ == '__main__':
     df = pd.read_csv(os.path.join(path, 'driving_log.csv'))
-    x , y = make_dataset(df)
+    x , y = make_dataset(df, False)
     with open("model.json", 'r') as jfile:
         model = model_from_json(jfile.read())
 
@@ -25,10 +26,21 @@ if __name__ == '__main__':
     
     #print(df.describe())
     
+    pred = []
+    actual = []
+    
     test_i = random.sample(range(x.shape[0]), 10)
     for i in test_i:
         img = x[i][None, : , : , :]
         #print(img.shape)
-        print("Prediction: ",str(float(model.predict(img, batch_size=1))))
-        print("Actual: ",str(y[i]))
+        pred.append(float(model.predict(img, batch_size=1)))
+        actual.append(y[i])
+    
+    plt.plot(pred)
+    plt.plot(actual)
+    plt.title('Prediction vs Actual')
+    plt.ylabel('Values')
+    plt.xlabel('Number of tests')
+    plt.legend(['Prediction', 'Actual'], loc='upper left')
+    plt.savefig('prediction.png')
     
